0d0ff325df7a011f75044a6c2d55a058d52a0696
JorgeVazquez20 <JorgeVazMen20@gmail.com>
2018-03-24 10:51:46 -0600

    LISTO

---
+++ jorge.h
@@ -0,0 +1,11 @@
+#ifndef JORGE_H_INCLUDE
+#define JORGE_H_INCLUDE
+
+#include <stdio.h>
+
+void jorgefunc()
+{
+	printf("Soy JORGE :) \n");
+}
+
+#endif
--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include"omar.h"
+#include"jorge.h"
 
 int main()
 {
 	omarfunc();
+	jorgefunc();
 }
