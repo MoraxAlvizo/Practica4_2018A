8654c92f298d4aba7eb03f082e42c059b2bc38b0
AlexisVazquez99 <daniel.vazqes@gmail.com>
2018-03-24 10:42:21 -0600

    Alexis Vazquez

---
+++ Alexis.h
@@ -0,0 +1,11 @@
+#ifndef Alexis_H_INCLUDE
+#define Alexis_H_INCLUDE
+
+#include <stdio.h>
+
+void Alexisfunc()
+{
+	printf("Soy Alexis :) \n");
+}
+
+#endif
--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include"omar.h"
+#include"Alexis.h"
 
 int main()
 {
 	omarfunc();
+	Alexisfunc();
 }
