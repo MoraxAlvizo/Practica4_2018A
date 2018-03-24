99d24933a95f8fd4cffe8d46abec6d37817a686f
agustindlcsanchez <guty2014@outlook.com>
2018-03-24 10:48:58 -0600

    Merge con Alex

---
+++ agustin.h
@@ -0,0 +1,11 @@
+#ifndef AGUSTIN_H_INCLUDE
+#define AGUSTIN_H_INCLUDE
+
+#include <stdio.h>
+
+void agustinfunc()
+{
+	printf("Soy Agustin :´( \n");
+}
+
+#endif
--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 #include"omar.h"
+#include"agustin.h"
+
 
 int main()
 {
 	omarfunc();
+	agustinfunc();
 }
