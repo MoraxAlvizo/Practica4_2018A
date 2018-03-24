9f5cff4a1cc2eea294cc22bc11b52a32e42abbee
Angel <asegura720@gmail.com>
2018-03-24 09:30:35 -0600

    Ya vamonos a la playita

---
+++ angel.h
@@ -0,0 +1,11 @@
+#ifndef ANGEL_H_INCLUDE
+#define ANGEL_H_INCLUDE
+
+#include <stdio.h>
+
+void angelfunc()
+{
+	printf("ewe ANGEL ewe \n");
+}
+
+#endif
--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include"omar.h"
+#include"angel.h"
 
 int main()
 {
 	omarfunc();
+	angelfunc();
 }
