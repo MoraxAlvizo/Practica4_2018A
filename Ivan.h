8e7009d30118cb2a1dd98e496882268df8097ff5
IvanLopezNeri <ivanlopezneri@gmail.com>
2018-03-24 10:39:11 -0600

    Ivan Perron

---
+++ Ivan.h
@@ -0,0 +1,11 @@
+#ifndef Ivan_H_INCLUDE
+#define Ivan_H_INCLUDE
+
+#include <stdio.h>
+
+void ivanfunc()
+{
+	printf("Soy Ivan :s \n");
+}
+
+#endif
--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 #include"omar.h"
+#include"Ivan.h"
+
 
 int main()
 {
 	omarfunc();
+	ivanfunc();
 }
