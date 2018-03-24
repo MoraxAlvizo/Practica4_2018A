82aedca19971784f46896ad25d5af2bd5c7779db
DanielCMX <daniel_caballero_07@hotmail.com>
2018-03-24 10:48:11 -0600

    hey

---
+++ Daniel.h
@@ -0,0 +1,11 @@
+#ifndef Daniel_H_INCLUDE
+#define Daniel_H_INCLUDE
+
+#include <stdio.h>
+
+void omarfunc()
+{
+	printf("Soy DANIEL D: \n");
+}
+
+#endif
--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include"omar.h"
+#include"Daniel.h"
 
 int main()
 {
 	omarfunc();
+	Danielfunc();
 }
