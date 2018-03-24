b9ed7001b26ac833ee1918f0c439ca08a1301bc2
ilecara <lifehouse24313@gmail.com>
2018-03-24 11:43:21 -0600

    hola

--- omar.h
+++ omar.h
@@ -1,28 +1,12 @@
 #ifndef OMAR_H_INCLUDE
-
-#ifndef ARACELI_H_INCLUDE
-
-#ifndef Paola_H_INCLUDE
-
 #define OMAR_H_INCLUDE
-#define Paola_H_INCLUDE
-#define ARACELI_H_INCLUDE
+
 
  #include <stdio.h>
  void omarfunc()
  {
 	 printf("Soy OMAR :) \n");
  }
-void ARACELIfunc()
- {
-	 printf("Soy ARACELI :) \n");
- }
-
-void paolafunc()
-{
-	printf("Soy Paola :) \n");
-}
-
 
 #endif
 
